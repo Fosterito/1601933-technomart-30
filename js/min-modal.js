2258210a748d90d2cdb862660e06d13aeb771ece
Fosterito <77629790+Fosterito@users.noreply.github.com>
2021-03-21 17:57:25 +0300

    Доработка защиты
    Добафил форматировыный файл js

---
+++ js/min-modal.js
@@ -0,0 +1,1 @@
+const btnModal=document.getElementById("btn-modal"),modal=document.getElementById("modal"),modalCloseBtn=document.getElementById("modal-close-btn");btnModal.addEventListener("click",()=>{modal.style.display="block"}),modalCloseBtn.addEventListener("click",()=>{modal.style.display="none"});
